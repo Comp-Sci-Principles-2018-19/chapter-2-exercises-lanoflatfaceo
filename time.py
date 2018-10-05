13921ceb4b2874975d8a80267b7e8ed098ae7bf3
lanoflatfaceo <43626792+lanoflatfaceo@users.noreply.github.com>
2018-10-05 10:37:37 -0400

    time v2

--- time.py
+++ time.py
@@ -1,3 +1,9 @@
 time=int(input("what time is it?"))
-actual_time=time+51
-print(actual_time)
+time_to_wait=int(input("How long do you want to wait?"))
+actual_time=time+time_to_wait
+time_v2=actual_time%24
+if time_v2<12:
+    print(time_v2,"A.M")
+else:
+    print(time_v2-12,"P.M.")
+    
